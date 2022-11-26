4b3038edf51b2d4a6235acb73e563b3463e58f0e
eskylo <neto.s@msn.com>
2022-11-26 17:54:33 -0300

    test

--- model/summoner.php
+++ model/summoner.php
@@ -10,13 +10,13 @@ class Summoner {
     private $summoner_level;
 
     function __construct($account_id, $profile_icon_id, $revision_date, $name, $id, $puuid, $summoner_level) {
-        $this->account_id = $account_id;
-        $this->profile_icon_id = $profile_icon_id;
-        $this->revision_date = $revision_date;
-        $this->name = $name;
-        $this->id = $id;
-        $this->puuid = $puuid;
-        $this->summoner_level = $summoner_level;
+        $this->{'account_id'} = $account_id;
+        $this->{'profile_icon_id'} = $profile_icon_id;
+        $this->{'revision_date'} = $revision_date;
+        $this->{'name'} = $name;
+        $this->{'id'} = $id;
+        $this->{'puuid'} = $puuid;
+        $this->{'summoner_level'} = $summoner_level;
     }
 }
 
