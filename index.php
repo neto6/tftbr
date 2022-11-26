230f6e344a9e01231f3fc02f2d806a214b5c697e
eskylo <neto.s@msn.com>
2022-11-26 17:30:53 -0300

    Test index

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Initial Index
+<?php
+
+require('../config/config.php');
+require('../model/summoner.php');
+require('../model/summoner_dao.php');
+
+
+$summoner_dao = new SummonerDAO();
+var_dump($summoner_dao->get_summoner($_GET['name']));
+
+?>
