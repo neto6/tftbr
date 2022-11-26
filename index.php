58db96164aa12a62f748b844877a42adc99d1d8c
eskylo <neto.s@msn.com>
2022-11-26 17:43:23 -0300

    test

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
+echo('hello world');
+
 require('./config/config.php');
 require('./model/summoner.php');
 require('./model/summoner_dao.php');
 
 
 $summoner_dao = new SummonerDAO();
-var_dump($summoner_dao->getSummoner('henper'));
+//var_dump($summoner_dao->getSummoner('henper'));
 
 ?>
