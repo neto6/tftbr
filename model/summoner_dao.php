06243c5115fb5b3657e923a816a71164d6adf75f
eskylo <neto.s@msn.com>
2022-11-26 17:35:59 -0300

    Test index

--- model/summoner_dao.php
+++ model/summoner_dao.php
@@ -17,6 +17,7 @@ class SummonerDAO {
 
     function getSummonerFromAPI($summoner_name) {
         global $end_point;
+        global $api_key;
         $response_body = file_get_contents(
             $end_point.'/tft/summoner/v1/summoners/by-name/'.$summoner_name.'?api_key='.$api_key
         );
